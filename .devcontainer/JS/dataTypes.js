3e952b61c87743ec09a5429fbc31abc05f18b6c9
RohaanCodes <115484289+RohaanCodes@users.noreply.github.com>
2024-06-13 11:18:14 +0000

    data types in js

---
+++ .devcontainer/JS/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat js code as the newer version of js
+
+//alert("hello") // we are using node.js not browser
+
+let name = "rohan";
+let age = 22;
+let islogedIn = true;
+
+
+// int , bigint, smallint
+//string = ""
+//boolen = true/false
+//null = stand alone value / empty value / temperature exampel
+// undefined = not assigned value
+//symbol = uniqueness
+
+
+//objects
+
+console.log(typeof null);
