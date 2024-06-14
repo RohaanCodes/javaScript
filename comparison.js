ba7c4983e994a938ad1e559c965a148e86a1bed7
RohaanCodes <muhammadrohan042002@gmai.com>
2024-06-14 15:18:34 +0000

    comapriosn file

---
+++ comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
+
+// first it convert null into number
+
+console.log("2" == 2);
+console.log("2" === 2);
+// === takes two paramete as separte data types
+
