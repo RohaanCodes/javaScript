5ef8f0e43d6af890c35b4921b6b674b89ca5155c
RohaanCodes <115484289+RohaanCodes@users.noreply.github.com>
2024-06-15 17:28:48 +0000

    de-structuring objects (ease to use again and again)

---
+++ JS_Basics02/objects-de-structured.js
@@ -0,0 +1,12 @@
+
+const courses = {
+
+    name: "DSA",
+    cousrseInstructor: "rohan",
+    courseDuration:"6 months"
+    
+}
+
+const {cousrseInstructor: instructor} = courses // deconstructing the object 
+
+console.log(instructor);
